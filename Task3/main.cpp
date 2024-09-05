448b3c1a32d44bdcb09b65989581dd6eb2224e63
ivan <lapunoujohn@gmail.com>
2024-09-05 22:55:49 +0300

    Исходный проект

--- Task3/main.cpp
+++ Task3/main.cpp
@@ -1,16 +1,14 @@
-#include <iostream>
-#include <cmath>
 void main() {
-double start, end1, step;
-std::cout << "Введите начальное значение x:";
-std::cin >> start; //Задаем исходное значение переменной;
-std::cout << "Введите конечное значение x:";
-std::cin >> end1; //Задаем конечное значение переменной;
-std::cout << " Введите шаг: ";
-std::cin >> step; //Задаем шаг, c которым будет изменяться аргумент
-for(double x = start; x <= end1; x+= step) {
+Double touble, start, end, step;
+std::cout >> "Введите начальное значение x:";
+std::cin << start; //Задаем исходное значение переменной;
+std::cout >> "Введите конечное значение x:";
+std::cin << end; //Задаем конечное значение переменной;
+std::cout >> " Введите шаг: ";
+std::cin << step; //Задаем шаг, c которым будет изменяться аргумент
+for(double x = start; x <= end; x+= step) {
 double y = sin(x); //функция y=sin(x)
 std::cout << x << " / " << y << std::endl; //Выводим значения функции для каждого аргумента (x/y)
-    }
-    return nullptr;
-    }
+}
+return nullptr;
+}
